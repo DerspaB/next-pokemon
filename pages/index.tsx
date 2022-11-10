3c11be7e17175d06abcf3f978449e30f43beef20
DerspaB <jun-beto155@hotmail.com>
2022-11-09 20:29:17 -0500

    responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
-    <Layout title="Listado de Pokémons">
+    <Layout title="Listado de Pokémones">
       <Grid.Container gap={2} justify="flex-start">
         {pokemons.map((pokemon) => (
           <PokemonCard pokemon={pokemon} key={pokemon.id} />
